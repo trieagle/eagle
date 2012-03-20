ab12a2f41febe1c2fc9e0d22b305515133f00754
soligho <qinxuzhen@gmail.com>
2012-03-20 21:18:18 +0800

    modified bitdemo/accounts

--- bitdemo/accounts/views.py
+++ bitdemo/accounts/views.py
@@ -1,4 +1,5 @@
 #coding=utf-8
+import re
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -45,10 +46,19 @@ def login(request):
     template_var["form"]=form        
     return render_to_response("accounts/login.html",template_var,context_instance=RequestContext(request))
     
-def _login(request,username,password):
+def _login(request,Name,password):
     '''登陆核心方法'''
     ret=False
-    user=authenticate(username=username,password=password)
+    #正则表达式匹配，用邮箱取得用户名
+    if re.match('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$',Name):
+        print "Email match"
+        try:
+            temp = User.objects.get(country="U.S.A.")
+        except ( User.MultipleObjectsReturned, User.DoesNotExist ):
+            ret = False
+        else:
+            Name = temp.name            
+    user=authenticate(username=Name,password=password)
     if user:
         if user.is_active:
             auth_login(request,user)
