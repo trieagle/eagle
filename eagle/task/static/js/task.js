16d12d7c56eed2b5a77b3e5e628b824cd1213094
wfwei <cf.wfwei@gmail.com>
2012-03-27 00:32:10 +0800

    task js

--- eagle/task/static/js/task.js
+++ eagle/task/static/js/task.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-  $("a").click(function(){
-    $("div.task-info").toggle();
-  });
+  $('.task-div').find('.task-info').hide().end().find('.task-title').click(function() {
+     $(this).parent().next().slideToggle();
+   });
+
+  $('.task-op').hide().parent().hover(function() {
+	$(this).children().next().append("Done?");
+	for ( var i = 1; i <= 5; i++ ) {
+		$(this).children().next().append("<a href='#'>" + i + "</a> ");
+	}
+
+	$(this).children().next().show(500).click(function(e) {
+		// stop normal link click
+		e.preventDefault();
+	})
+	},
+	function() {
+		$(this).children().next().empty();
+	}
+
+  );
 });
             
