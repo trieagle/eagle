de4afd04f2183998548ee893cbe7e94bf6044f72
skyshaw <pxtian2008@gmail.com>
2012-03-29 23:13:35 +0800

    finish edit

--- eagle/task/static/js/task.js
+++ eagle/task/static/js/task.js
@@ -6,35 +6,53 @@ $(document).ready(function() {
 
 
     $('.task-op').find('.edit').click(function() {
-/*        task = $(this).parent().parent().parent();
-        taskTags = task.find('task-tags');
-
-        taskObj = {
-            title: task.find('.task-titile').html(),
-            detail: task.find('.task-detail').html(),
-            priority: task.find('.task-priority').html(),
-            tags: tagList
-        };
-
-        $.ajax({
-            url: '/',
-            type: 'post',
-            dataType: 'json',
-            data: taskObj
-            success: function(revTaskObj) {
-
-            }
-        });*/
         task = $(this).parent().parent().parent();
 
         taskTitle = task.find('.task-title');
         taskTitle.hide().after(
             '<input class="task-title-input" value="' 
             + taskTitle.html() + '" >');
+
         taskDetail = task.find('.task-detail');
         taskDetail.hide().after(
             '<input class="task-detail-input" value="' 
             + taskDetail.html() + '" >');
+
+        editOption ='<div class="edit-op">'
+            + '<button class="confirm">confirm</button>'
+            + '<button class="cancel">cancel</button>'
+			+ '</div>';
+
+        taskOp = task.find('.task-op');
+        taskOp.hide().after(editOption);
+
+        task.find('.edit-op').find('.confirm').click(function() {
+            taskTags = task.find('task-tags');
+
+            taskObj = {
+                title: task.find('.task-title-input').value(),
+                detail: task.find('.task-detail-input').value(),
+            };
+
+            $.ajax({
+                url: '/',
+                type: 'post',
+                dataType: 'json',
+                data: JSON.stringify(taskObj),
+                success: function(revTaskObj) {
+                    taskTitle.html(revTaskObj.title).show()
+                        .next().hide();
+                    taskDetail.html(revTaskObj.detail).show()
+                        .next().hide();
+                    taskOp.show().next().hide();
+                }
+            });
+        })
+        .end().find('.cancel').click(function() {
+            taskTitle.show().next().hide();
+            taskDetail.show().next().hide();
+            taskOp.show().next().hide();
+        });
 
     }).end().find('.delete').click(function() {
         task = $(this).parent().parent().parent();
